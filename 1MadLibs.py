3e5337e6d3348f0d83017a5727ccfaf435ea81db
Tashil <128675280+Tashil70@users.noreply.github.com>
2024-03-09 15:00:51 +0200

    Add files via upload

---
+++ 1MadLibs.py
@@ -0,0 +1,9 @@
+adj = input("Please enter an adjective: ")
+verb1 = input("Please enter a verb: ")
+verb2 = input("Please enter another verb: ")
+famousPerson = input("Please enter a foumous person: ")
+
+madLib=f"Computer Programming is so {adj}! It makes me so exited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famousPerson}!"
+
+print(madLib)
